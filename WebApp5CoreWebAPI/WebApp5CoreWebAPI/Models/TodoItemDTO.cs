6660d12090e11eef8c9a6651c9fcec2802620bdb
sanjeevg77-max <sanjeevg77@gmail.com>
2026-06-29 00:16:18 +0530

    Delete WebApp5CoreWebAPI directory

--- WebApp5CoreWebAPI/WebApp5CoreWebAPI/Models/TodoItemDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApp5CoreWebAPI.Models
-{
-    public class TodoItemDTO
-    {
-        public long Id { get; set; }
-        public string? Name { get; set; }
-        public bool IsComplete { get; set; }
-    }
-}
